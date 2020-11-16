9b22bd3dea37acb485bc5ec45ae82bdb5762f47b
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-11-16 18:50:07 +0300

    Initial Commit

---
+++ Test GAM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test GAM
+//
+//  Created by Sylvan Ash on 16/11/2020.
+//  Copyright © 2020 Sylvan Ash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
