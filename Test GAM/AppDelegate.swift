763c1cd5eda838ca3e5e182b7ca1f4442fa3720c
Sylvan Ash <sylvan.d.ash@gmail.com>
2022-01-04 23:10:48 +0300

    Add OM SDK

--- Test GAM/AppDelegate.swift
+++ Test GAM/AppDelegate.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import OMSDK_Bleacherreport
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        initializeOMSDK()
         return true
     }
 
@@ -22,5 +23,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
+
+extension AppDelegate {
+    func initializeOMSDK() {
+        if OMIDBleacherreportSDK.shared.isActive { return }
+        OMIDBleacherreportSDK.shared.activate()
     }
 }
