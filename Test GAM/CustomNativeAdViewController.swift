f763c71ffda398dcdafc2d80d27ee8ad68d6ca01
Sylvan Ash <sylvan.d.ash@gmail.com>
2022-10-24 19:32:58 +0300

    Load and display the accordion ad

--- Test GAM/CustomNativeAdViewController.swift
+++ Test GAM/CustomNativeAdViewController.swift
@@ -146,7 +146,29 @@ extension CustomNativeAdViewController: GADCustomNativeAdLoaderDelegate {
     }
 
     func adLoader(_ adLoader: GADAdLoader, didReceive customNativeAd: GADCustomNativeAd) {
-       //
+        guard let templateId = NativeAdTemplateId(rawValue: customNativeAd.formatID) else {
+            loadStatusLabel.text = "Ad Status: Can't parse loaded ad ⚠️"
+            return
+        }
+
+        switch templateId {
+        case .accordion:
+            adView = CustomNativeAccordionAdView(ad: customNativeAd)
+        default:
+            // TODO
+            loadStatusLabel.text = "Ad Status: Loaded but not implemented 💠"
+        }
+
+        if let adView = adView {
+            containerView.addSubview(adView)
+            adView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                adView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                adView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                adView.topAnchor.constraint(equalTo: containerView.topAnchor),
+                adView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            ])
+        }
     }
 }
 
@@ -227,4 +249,114 @@ private enum NativeAdTemplateId: String {
     case devContent = "10075449"
     case devAppInstall = "10075569"
     case accordion = "11910332"
+}
+
+private class CustomNativeAccordionAdView: UIView {
+    private var ad: GADCustomNativeAd?
+
+    init?(ad: GADCustomNativeAd) {
+        guard let image = ad.image(forKey: "image")?.image else {
+            return nil
+        }
+        super.init(frame: .zero)
+        setupSubviews(image)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupSubviews(_ image: UIImage) {
+        let image = resizeAdImage(image)
+        let imageView = UIImageView(image: image)
+        addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: scaledMaxHeight),
+        ])
+    }
+
+    private func resizeAdImage(_ image: UIImage) -> UIImage {
+        var img = image
+        var renderedSize = CGSize(width: scaledWidth, height: scaledMaxHeight)
+        if !(image.size.equalTo(renderedSize) && image.scale == UIScreen.main.scale) {
+            img = img.resized(to: renderedSize) ?? img
+        }
+        return img
+    }
+
+    private var nativeWidth: CGFloat {
+        return ViewHelper.panelWidth * UIScreen.main.scale
+    }
+
+    private var scaledWidth: CGFloat {
+        return nativeWidth / UIScreen.main.scale
+    }
+
+    private let aspectRatio: CGFloat = 1
+
+    private var nativeMaxHeight: CGFloat {
+        // use ceil to avoid leftover pixels when resizing
+        return ceil(nativeWidth / aspectRatio)
+    }
+
+    private var scaledMaxHeight: CGFloat {
+        return nativeMaxHeight / UIScreen.main.scale
+    }
+}
+
+private extension UIImage {
+    /**
+     Returns a resized image using the specified size and quality.
+
+     - parameter size: The size to use when resizing the image.
+     - parameter quality: The desired quality. The default value is `CGInterpolationQuality.high`
+     - returns: A resize image if one can be created. Otherwise, nil.
+    */
+    func resized(to size: CGSize, using quality: CGInterpolationQuality = .high) -> UIImage? {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        guard let cgImage = cgImage else { return nil }
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        let context =  UIGraphicsGetCurrentContext()
+
+        context?.interpolationQuality = quality
+        let transform = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: size.height)
+        context?.concatenate(transform)
+
+        context?.draw(cgImage, in: rect)
+        guard let resizedCGImage = context?.makeImage() else { return nil }
+        let image = UIImage(cgImage: resizedCGImage, scale: UIScreen.main.scale, orientation: imageOrientation)
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
+
+private class ViewHelper: NSObject {
+    static let maxPanelWidth: CGFloat = 504
+
+    static var isNarrowPhone: Bool {
+        return windowWidth < 375
+    }
+
+    // Only use this if you're going to re-check the value on every appWillChangeSize call
+    static var panelWidth: CGFloat {
+        return min(windowWidth, maxPanelWidth)
+    }
+
+    // Only use this if you're going to re-check the value on every appWillChangeSize call
+    static var panelPadding: CGFloat {
+        return panelWidth >= windowWidth ? 0 : ((windowWidth - maxPanelWidth) / 2)
+    }
+
+    static var windowWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+
+    static var windowHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
 }
